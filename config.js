81bf856645a30d3a752f95e91bfeba8ccbb565c3
JoaoPiccolli <135072261+JoaoPiccolli@users.noreply.github.com>
2024-06-16 17:36:07 -0300

    Add files via upload

--- config.js
+++ config.js
@@ -1,8 +1,13 @@
 import firebase from 'firebase';
 
 // adicione SDK do Firebase
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA0fi-fgd0P0iV9Li9AzsYNTRwCp7dnTz4",
+  authDomain: "contribuindo-com-projetos.firebaseapp.com",
+  projectId: "contribuindo-com-projetos",
+  storageBucket: "contribuindo-com-projetos.appspot.com",
+  messagingSenderId: "988747932556",
+  appId: "1:988747932556:web:3b0cd89e09175525cf6ca9"
 };
 // Initialice o Firebase
 firebase.initializeApp(firebaseConfig);
